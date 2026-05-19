59559a1d4fea7e818434c91234546eafac4dd5fa
Rupesh Kumar <therupeshkumar8825@gmail.com>
2026-05-19 21:59:45 +0530

    Working on the single product page

--- client/src/pages/SingleProductDetailPage.tsx
+++ client/src/pages/SingleProductDetailPage.tsx
@@ -26,27 +26,30 @@ export const SingleProductDetailPage = () => {
             setCurrProductDetails(product? product : null)
         }
     }, [useParamObject])
+
+
+    // the actual jsx/ui component related code comes here 
     return (
         <div className="border border-black flex flex-col justify-center items-center">
             {/* the path heading */}
-            <div className="border border-red-500">
+            <div className="border border-red-500 py-5">
                 here comes the top heading meaning the path for this purpose
             </div>            
             {/* actual product details comes here */}
-            <div className="border-2 border-black flex flex-row justify-center items-center">
+            <div className="flex flex-row gap-10 justify-center items-top mt-10 mb-10">
                 {/* product image comes here  */}    
-                <div className="border-2 border-red-500">
-                    <img src="some imagee comews here" alt="some image comes here" />
+                <div className="rounded-xl">
+                    <img className="h-[90%] w-[90%]" src={currProductDetails?.image} alt={currProductDetails?.title} />
                 </div>
 
                 {/* the complete product details comes here */}
-                <div className="border-2 border-yellow-500 flex flex-col justify-center items-center">
+                <div className="flex flex-col justify-around items-center px-10 py-10 w-[40%] rounded-xl shadow-2xl">
                     {/* top heading of the product comes here  */}
                     <h1 className="font-bold text-3xl">{currProductDetails?.title}</h1>
                     <p>{currProductDetails?.category}</p>
 
                     {/* price related information comes here */}
-                    <div className="border-2 border-orange-500 flex flex-row justify-center items-center">
+                    <div className="flex flex-row justify-center items-center">
                         <p>price 1 </p>
                         <p>price 2</p>
                         <button className="text-white px-3 py-2 bg-red-500 font-semibold">4% Discount</button>
@@ -56,15 +59,15 @@ export const SingleProductDetailPage = () => {
                     <p>{currProductDetails?.description}</p>
 
                     {/* product quantity related details comes here */}
-                    <div className="border-2 border-black flex flex-row justify-center items-center">
+                    <div className="flex flex-row justify-center items-center">
                         <p>Quantity</p>
                         <input type="text" value={1} placeholder="1" />
                     </div>
 
                     {/* finally comes the add to cart button for this purpose */}
-                    <button className="text-white bg-red-500 flex flex-row justify-center items-center px-3 py-2">
+                    <button className="text-white bg-red-500 flex flex-row justify-center items-center px-3 py-2 shadow-3xl shadow-amber-400">
                        <IoCartOutline></IoCartOutline>
-                        <p>Add to Cart</p>
+                        <p className="shadow-4xl shadow-black">Add to Cart</p>
                     </button>
                 </div>
             </div>
