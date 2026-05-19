c4b2e9cc1969cb3f896afcad7b1a9d0d80b5190c
Rupesh Kumar <therupeshkumar8825@gmail.com>
2026-05-19 22:33:55 +0530

    Complete the single page product too

--- client/src/pages/SingleProductDetailPage.tsx
+++ client/src/pages/SingleProductDetailPage.tsx
@@ -18,6 +18,7 @@ export const SingleProductDetailPage = () => {
 
     // all the states related to the component comes here 
     const [currProductDetails, setCurrProductDetails] = useState<ProductDetail | null>(null);
+    const [notDiscountedPrice, setNotDiscountedPrice] = useState<number>(0);
 
     useEffect(() => {
         if(useParamObject){
@@ -27,14 +28,17 @@ export const SingleProductDetailPage = () => {
         }
     }, [useParamObject])
 
+    useEffect(() => {
+        // lets calculate the not discounted price for this purpose
+    }, [])
 
     // the actual jsx/ui component related code comes here 
     return (
-        <div className="border border-black flex flex-col justify-center items-center">
+        <div className="flex flex-col justify-center items-center">
             {/* the path heading */}
-            <div className="border border-red-500 py-5">
+            {/* <div className="border border-red-500 py-5">
                 here comes the top heading meaning the path for this purpose
-            </div>            
+            </div>             */}
             {/* actual product details comes here */}
             <div className="flex flex-row gap-10 justify-center items-top mt-10 mb-10">
                 {/* product image comes here  */}    
@@ -43,29 +47,30 @@ export const SingleProductDetailPage = () => {
                 </div>
 
                 {/* the complete product details comes here */}
-                <div className="flex flex-col justify-around items-center px-10 py-10 w-[40%] rounded-xl shadow-2xl">
+                <div className="flex flex-col justify-around items-left px-10 py-10 w-[40%] rounded-xl shadow-2xl">
                     {/* top heading of the product comes here  */}
                     <h1 className="font-bold text-3xl">{currProductDetails?.title}</h1>
-                    <p>{currProductDetails?.category}</p>
+                    <p className="uppercase  text-md ">{currProductDetails?.category}</p>
 
                     {/* price related information comes here */}
-                    <div className="flex flex-row justify-center items-center">
-                        <p>price 1 </p>
-                        <p>price 2</p>
-                        <button className="text-white px-3 py-2 bg-red-500 font-semibold">4% Discount</button>
+                    <div className="flex flex-row gap-2 justify-left items-center">
+                        <p className="text-red-500 font-semibold">${currProductDetails?.price}</p>
+                        <p className="font-semibold line-through">${currProductDetails? Math.round(currProductDetails.price*100/96) : null}</p>
+                        <button className="text-white px-3 py-2 bg-red-500 rounded-md">4% Discount</button>
                     </div>
 
                     {/* actual product description comes here */}
-                    <p>{currProductDetails?.description}</p>
+                    <p className="text-md w-[85%] mt-5 mb-5">{currProductDetails?.description}</p>
 
                     {/* product quantity related details comes here */}
-                    <div className="flex flex-row justify-center items-center">
+                    <div className="flex flex-row justify-left items-center">
                         <p>Quantity</p>
-                        <input type="text" value={1} placeholder="1" />
+                        <input type="text" placeholder="1" className="ml-2 w-15 px-4 py-2 bg-white/20 border border-gray-400 text-black rounded-xl " />
+
                     </div>
 
                     {/* finally comes the add to cart button for this purpose */}
-                    <button className="text-white bg-red-500 flex flex-row justify-center items-center px-3 py-2 shadow-3xl shadow-amber-400">
+                    <button className="mt-5 w-[40%] text-white bg-red-500 flex flex-row justify-center items-center px-3 py-2 shadow-2xl rounded-sm cursor-pointer">
                        <IoCartOutline></IoCartOutline>
                         <p className="shadow-4xl shadow-black">Add to Cart</p>
                     </button>
